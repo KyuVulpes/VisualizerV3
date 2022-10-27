460c989e7dd1b41da53f4bd1d416215a2b778dae
Kyu Vulpes <kuroneko.felis@gmail.com>
2022-10-27 18:13:45 -0500

    Changed IVisualizeShape to VisualizerShape and made it an abstract class. Also added a few helper methods and properties.

--- Assets/Scripts/Assembly-CSharp/Visual/Shapes/IVisualizeShape.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-
-namespace VisualizerV3.Visual.Shapes {
-	public interface IVisualizeShape {
-		void GenerateShape( in int count, in float radius, in Vector3 parentPos, out Vector3[] posArray, out Quaternion[] rotationArray, out int[] barNums );
-	}
-}
---
+++ Assets/Scripts/Assembly-CSharp/Visual/Shapes/VisualizerShape.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+namespace VisualizerV3.Visual.Shapes {
+	/// <summary>
+	/// Used to make a consistent way to build a visualizer pattern.
+	/// </summary>
+	public abstract class VisualizerShape {
+		protected static float Tau {
+			get => Mathf.PI * 2;
+		}
+
+		protected static Vector3 GetXAndY( float radius, float theta ) {
+			var x = radius * Mathf.Sin( theta );
+			var y = radius * Mathf.Cos( theta );
+
+			return new Vector3( x, y );
+		}
+
+		/// <summary>
+		/// Used to build the shape of the visualizer.
+		/// </summary>
+		/// <remarks>
+		/// This method is used internally to build a visualizer. You should never have to call this method.
+		/// This method is meant to keep the implementation simple and not complex at all.
+		///
+		/// It is always best to fill up the <paramref name="barNums"/> with appropriate values, the reason it
+		/// exists and needs to be filled is in cases where the bar count is bigger than <paramref name="count"/>.
+		/// For example, when making a mirrored circle:
+		/// <code>
+		///	
+		/// </code>
+		/// </remarks>
+		/// <param name="count">The number of visualizer bars that are needed.</param>
+		/// <param name="radius">How big the circle is.</param>
+		/// <param name="parentPos">Where the center is.</param>
+		/// <param name="posArray">An array of positions to place the reactors.</param>
+		/// <param name="rotationArray">An array of rotations to rotate the reactors.</param>
+		/// <param name="barNums">An array saying what each bar number is.</param>
+		public abstract void GenerateShape( in int count, in float radius, in Vector3 parentPos, out Vector3[] posArray, out Quaternion[] rotationArray, out int[] barNums );
+	}
+}
