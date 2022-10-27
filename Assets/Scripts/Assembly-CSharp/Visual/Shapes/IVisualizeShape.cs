94ee9f6c91fda83e71e2ef9a6ba9a5367fed0f21
Kyu Vulpes <kuroneko.felis@gmail.com>
2022-10-27 17:57:43 -0500

    Relocated the IVisualizeShape class to the proper namespace. (Also moved the .meta file because Unity.)

--- Assets/Scripts/Assembly-CSharp/Visual/Shapes/IVisualizeShape.cs
+++ Assets/Scripts/Assembly-CSharp/Visual/Shapes/IVisualizeShape.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 
-namespace VisualizerV3.Audio {
+namespace VisualizerV3.Visual.Shapes {
 	public interface IVisualizeShape {
 		void GenerateShape( in int count, in float radius, in Vector3 parentPos, out Vector3[] posArray, out Quaternion[] rotationArray, out int[] barNums );
 	}
