f9af9e2f796da7875f509424bde4ea38c0b5102f
Kyu Vulpes <kuroneko.felis@gmail.com>
2022-10-27 17:58:01 -0500

    Fixed namespace issues in other related classes.

--- Assets/Scripts/Assembly-CSharp/Visual/Shapes/MirroredCircle.cs
+++ Assets/Scripts/Assembly-CSharp/Visual/Shapes/MirroredCircle.cs
@@ -1,7 +1,7 @@
 ﻿using System.Text;
 using UnityEngine;
 
-namespace VisualizerV3.Audio.Shapes {
+namespace VisualizerV3.Visual.Shapes {
 	public class MirroredCircle : IVisualizeShape {
 
 		public void GenerateShape( in int count, in float radius, in Vector3 parentPos, out Vector3[] posArray, out Quaternion[] rotationArray, out int[] barNums ) {
--- Assets/Scripts/Assembly-CSharp/Visual/VisualizerManager.cs
+++ Assets/Scripts/Assembly-CSharp/Visual/VisualizerManager.cs
@@ -6,7 +6,7 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Assertions;
 using UnityEngine.Serialization;
-using VisualizerV3.Audio.Shapes;
+using VisualizerV3.Visual.Shapes;
 
 namespace VisualizerV3.Audio {
 	public class VisualizerManager : MonoBehaviour {
