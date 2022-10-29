a327ed9c97991c819ea03f0d093c4a2205d39c3d
Kyu Vulpes <kuroneko.felis@gmail.com>
2022-10-29 17:52:57 -0500

    Made it easier for external classes to access the fields.

--- Assets/Scripts/Assembly-CSharp/Shared/ObjectData.cs
+++ Assets/Scripts/Assembly-CSharp/Shared/ObjectData.cs
@@ -4,6 +4,25 @@ using UnityEngine;
 
 namespace VisualizerV3.Shared {
 	public class ObjectData : MonoBehaviour {
+		public string Author {
+			get => author;
+		}
+
+		public string Version {
+			get => version;
+		}
+
+		public string Copyright {
+			get => copyright;
+		}
+
+		public string ProjectName {
+			get => projectName;
+		}
+
+		public Texture2D Icon {
+			get => icon;
+		}
 
 		[SerializeField]
 		private string author;
