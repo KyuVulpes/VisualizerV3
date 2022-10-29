a52816ed3e262062d1bfe0f237bffbdbad6593f7
Kyu Vulpes <kuroneko.felis@gmail.com>
2022-10-29 16:50:26 -0500

    Added the hover and hover editor.

---
+++ Assets/Scripts/Assembly-CSharp/Shared/Hover.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Serialization;
+using VisualizerV3.Audio;
+
+namespace VisualizerV3.Shared {
+	public class Hover : MonoBehaviour {
+		public enum FloatingType : byte {
+			Sin = 0,
+			Saw = 1,
+		}
+
+		[SerializeField]
+		private bool reactToAudio;
+		[SerializeField]
+		private float speed = 1f;
+		[SerializeField]
+		private float height = 1f;
+		[SerializeField]
+		private float reactionDamp;
+		private float time;
+
+		[SerializeField]
+		private AudioProcessor.BandFreq band;
+		[FormerlySerializedAs( "floatType" )]
+		[SerializeField]
+		private FloatingType floatingType;
+
+		private Vector3 basePos;
+		
+		private AudioProcessor processor;
+
+		private void Awake() {
+			basePos = transform.position;
+		}
+
+		private void Start() {
+			processor = AudioProcessor.MainProcessor;
+		}
+
+		private void Update() {
+			time += Time.deltaTime;
+
+			var x = time * speed;
+
+			if ( reactToAudio ) {
+				x += processor.GetFreqBand( band ) / reactionDamp;
+			}
+
+			var upDist = floatingType switch {
+				FloatingType.Saw => 2 / Mathf.PI * Mathf.Asin( Mathf.Sin( Mathf.PI * x ) ) * height,
+				FloatingType.Sin => Mathf.Sin( x ) * height,
+				_                => throw new ArgumentOutOfRangeException( nameof( floatingType ), floatingType, "Expected the floating type to be known." )
+			};
+
+			var offset = new Vector3( 0f, upDist );
+
+			transform.position = basePos + offset;
+		}
+	}
+}
---
+++ Assets/Scripts/Editor/CustomInspectorEditors/HoverEditor.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using UnityEditor;
+using VisualizerV3.Audio;
+using VisualizerV3.Shared;
+
+namespace VisualizerV3.Editor {
+	[CustomEditor( typeof(Hover) )]
+	[CanEditMultipleObjects]
+	public class HoverEditor : UnityEditor.Editor {
+		private float falseHeight;
+
+		private SerializedProperty react;
+		private SerializedProperty speed;
+		private SerializedProperty height;
+		private SerializedProperty reactDamp;
+		private SerializedProperty band;
+		private SerializedProperty floatingType;
+
+		private void OnEnable() {
+			react        = serializedObject.FindProperty( "reactToAudio" );
+			speed        = serializedObject.FindProperty( "speed" );
+			height       = serializedObject.FindProperty( "height" );
+			reactDamp    = serializedObject.FindProperty( "reactionDamp" );
+			band         = serializedObject.FindProperty( "band" );
+			floatingType = serializedObject.FindProperty( "floatingType" );
+
+			falseHeight = height.floatValue / 2f;
+		}
+
+		public override void OnInspectorGUI() {
+			serializedObject.Update();
+
+			EditorGUILayout.PropertyField( speed );
+
+			falseHeight = EditorGUILayout.Slider( "Height", falseHeight, 0.25f, 5f );
+			
+			floatingType.enumValueIndex = ( byte )( Hover.FloatingType )EditorGUILayout.EnumPopup( "Band", ( Hover.FloatingType )Enum.GetValues( typeof(Hover.FloatingType) ).GetValue( floatingType.enumValueIndex ) );
+			
+			EditorGUILayout.Separator();
+			EditorGUILayout.PropertyField( react );
+
+			if ( react.boolValue ) {
+				reactDamp.floatValue = EditorGUILayout.Slider( "Dampen Audio Value", reactDamp.floatValue, 10f, 30f );
+				band.enumValueIndex  = ( byte )( AudioProcessor.BandFreq )EditorGUILayout.EnumPopup( "Band", ( AudioProcessor.BandFreq )Enum.GetValues( typeof(AudioProcessor.BandFreq) ).GetValue( band.enumValueIndex ) );
+			}
+
+			height.floatValue = falseHeight * 2;
+
+			if ( speed.floatValue < 1f ) {
+				speed.floatValue = 1f;
+			}
+
+			serializedObject.ApplyModifiedProperties();
+		}
+	}
+}
