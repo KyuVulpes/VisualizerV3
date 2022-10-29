7e7713374883f9a0c5bccdb9f02ba5ac88af7565
Kyu Vulpes <kuroneko.felis@gmail.com>
2022-10-29 14:54:42 -0500

    Created a MonoBehaviour script that will be removed before making an object an AssetBundle.

---
+++ Assets/Scripts/Assembly-CSharp/Information/ObjectData.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VisualizerV3.Information {
+	public class ObjectData : MonoBehaviour {
+
+		[SerializeField]
+		private string author;
+		[SerializeField]
+		private string version;
+		[SerializeField]
+		private string copyright;
+
+		[SerializeField]
+		private Texture2D icon;
+
+	}
+}
